1a7c229c0d6e132d29ae52dd74dd86b7ce99c6fe
Hassan <hassangulzar121@gmail.com>
2024-03-23 22:20:34 +0500

    working on api

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client";
-//import axios from "axios"; // Import axios for making HTTP requests
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IoMdAttach } from "react-icons/io";
 import { ThreeDots } from "react-loader-spinner";
 import Spinner from "../components/loader/spinner";
@@ -8,50 +7,171 @@ import Image from "next/image";
 import AiProfile from "../Assets/images/aiLogo.png";
 import userProfile from "../Assets/images/user.jpg";
 
+const promptsArray = [
+  "Enter your event name",
+  "Enter your event description",
+  "Enter Your event goal",
+];
+
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [prompt, setPrompt] = useState("");
   const [promptsArr, setPromptsArr] = useState([]);
-  const [recentAnswer, setRecentAnswer] = useState("");
-  const [error, setError] = useState(null);
+
+  const [eventsDetail, setEventsDetail] = useState({
+    eventName: "",
+    eventDesc: "",
+    formGoal: "",
+  });
+
+  //! if User not exist
+  const creatingNewUser = (e) => {
+    if (userName == null) {
+      setPromptsArr([...promptsArr, "Enter your name"]);
+    } else {
+      setPromptsArr([...promptsArr, e.target.value]);
+    }
+  };
+  //! if user exist but event detail not available
+  //   const getEventDetails = () => {
+  //     if (eventsDetail.eventName == "") {
+  //       setPromptsArr([...promptsArr, "Enter Your Event Name"]);
+  //     } else if (
+  //       eventsDetail.eventName != "" &&
+  //       eventsDetail.eventDescription == ""
+  //     ) {
+  //       setPromptsArr([...promptsArr, "Enter Your Event Description"]);
+  //     } else {
+  //       setPromptsArr([...promptsArr, "Enter Your Form goal"]);
+  //     }
+  //
+  //     if (
+  //       eventsDetail.eventName != "" &&
+  //       eventsDetail.eventDescription != "" &&
+  //       eventsDetail.formGoal != ""
+  //     ) {
+  //       const myHeaders = new Headers();
+  //       myHeaders.append("Content-Type", "application/json");
+  //
+  //       const raw = JSON.stringify(eventsDetail);
+  //       const requestOptions = {
+  //         method: "POST",
+  //         headers: myHeaders,
+  //         body: raw,
+  //         redirect: "follow",
+  //       };
+  //       fetch(
+  //         "https://gemini-ai-hackathon-efa-backend.onrender.com/organizers/events/new",
+  //         requestOptions
+  //       )
+  //         .then((response) => response.text())
+  //         .then((result) => console.log(result))
+  //         .catch((error) => console.error(error));
+  //     }
+  //   };
+
+  //! if user already Exists.
+  //   const userExistOrNot = () => {
+  //     let id1 = localStorage.getItem("id1");
+  //     let id2 = localStorage.getItem("id2");
+  //   };
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  async function postData(url, options) {
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (prompt.trim() !== "") {
-      console.log("prompt -> ", prompt);
+    let id1 = localStorage.getItem("id1");
+    let id2 = localStorage.getItem("id2");
+
+    if (id1 === null) {
       setPromptsArr([...promptsArr, prompt]);
-      const postData = {
-        query:
-          prompt +
-          "? Give me response in JSON & there should be 1 key named response",
-      };
       setPrompt("");
+      const raw = JSON.stringify({
+        userName: prompt,
+      });
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
 
-      setLoading(true);
-      // API Calling and getting response code
-      try {
-        // Make a POST request using axios
-        const response = await axios.post(
-          "https://ecofactory.onrender.com/api/eco_fac_gpt",
-          postData
-        );
+      let res = await postData(
+        "https://gemini-ai-hackathon-efa-backend.onrender.com/users/",
+        requestOptions
+      );
 
-        console.log("response.data -> ", response.data);
-        setRecentAnswer(response.data.response);
-        setPromptsArr((prevPromptsArr) => [
-          ...prevPromptsArr,
-          response?.data?.response,
-        ]);
-        setLoading(false);
-        setError(null); // Reset error state
-      } catch (error) {
-        // Handle errors
-        console.error("Error fetching data: ", error);
-        setError(error.message); // Store error message in state
-        setLoading(false);
-      }
+      console.log(res);
+    } else if (id1 != null && id2 === null) {
+      setPromptsArr([...promptsArr, prompt]);
+      setPrompt("");
+      setEventsDetail([]);
     }
+
+    // e.preventDefault();
+    //     if (prompt.trim() !== "") {
+    //       console.log("prompt -> ", prompt);
+    //       setPromptsArr([...promptsArr, prompt]);
+    //       const postData = {
+    //         query:
+    //           prompt +
+    //           "? Give me response in JSON & there should be 1 key named response",
+    //       };
+    //       setPrompt("");
+    //
+    //       setLoading(true);
+    //       // API Calling and getting response code
+    //
+    //       try {
+    //         // Make a POST request using axios
+    //         const response = fetch(
+    //           "https://ecofactory.onrender.com/api/eco_fac_gpt",
+    //           postData
+    //         );
+    //
+    //         console.log("response.data -> ", response.data);
+    //         setRecentAnswer(response.data.response);
+    //         setPromptsArr((prevPromptsArr) => [
+    //           ...prevPromptsArr,
+    //           response?.data?.response,
+    //         ]);
+    //         setLoading(false);
+    //         setError(null); // Reset error state
+    //       } catch (error) {
+    //         // Handle errors
+    //         console.error("Error fetching data: ", error);
+    //         setError(error.message); // Store error message in state
+    //         setLoading(false);
+    //       }
+    //     }
   };
+
+  useEffect(() => {
+    let id1 = localStorage.getItem("id1");
+    let id2 = localStorage.getItem("id2");
+
+    localStorage.setItem("id1", "hassan");
+
+    if (id1 === null) {
+      setPromptsArr([...promptsArr, "Enter Your Name"]);
+    } else if (id1 != null && id2 === null) {
+      let particularPrompt =
+        eventsDetail.eventName == "" ? 0 : eventsDetail.eventDesc == "" ? 1 : 2;
+
+      setPromptsArr([...promptsArr, promptsArray[particularPrompt]]);
+    }
+  }, []);
 
   return (
     <div
@@ -79,7 +199,7 @@ export default function Home() {
               height: "96vh",
               display: "flex",
               flexDirection: "column",
-              border: "2px solid blue",
+              border: "1px solid white",
               borderRadius: "25px",
             }}
           >
@@ -144,7 +264,7 @@ export default function Home() {
                     <div className="container-fluid">
                       {promptsArr.map((prom, index) => (
                         <div key={index}>
-                          {index % 2 === 0 ? (
+                          {index % 2 != 0 ? (
                             <div
                               className="row p-0 m-0 "
                               style={{
@@ -197,7 +317,6 @@ export default function Home() {
                           }}
                         >
                           <img
-                            src={chatgptLogo}
                             className="rounded-circle mb-auto me-2"
                             style={profileStyle}
                             alt=""
